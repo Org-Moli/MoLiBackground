c0a0a6c52de0841555ed2cee8e5f339702d1d3bf
coralandbill <347135219@qq.com>
2017-03-16 16:11:22 +0800

    驾驶员申请|详情 驾驶员列表|详情 静态页面

---
+++ wk-app/wk-web/src/main/java/cn/wizzer/app/web/modules/controllers/platform/dirver/DriverManageController.java
@@ -0,0 +1,48 @@
+package cn.wizzer.app.web.modules.controllers.platform.dirver;
+
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.Ok;
+
+/**
+ * <p>名称</p>
+ * <p/>
+ * <p>wikiURL</p>
+ *
+ * @author zb.jiang
+ * @version 1.0
+ * @Date 2017/3/16
+ */
+@IocBean
+@At("/platform/driver")
+public class DriverManageController {
+
+    @At("/apply")
+    @Ok("beetl:/platform/driver/apply.html")
+    @RequiresAuthentication
+    public void apply() {
+
+    }
+
+    @At("/viewDriver/?")
+    @Ok("beetl:/platform/driver/view.html")
+    @RequiresAuthentication
+    public void viewDriver() {
+
+    }
+
+    @At("/drivers")
+    @Ok("beetl:/platform/driver/drivers.html")
+    @RequiresAuthentication
+    public void drivers() {
+
+    }
+
+    @At("/viewDriverInfo/?")
+    @Ok("beetl:/platform/driver/viewInfo.html")
+    @RequiresAuthentication
+    public void viewDriverInfo() {
+
+    }
+}
